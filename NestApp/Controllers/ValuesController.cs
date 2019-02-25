777420e6203e17b2e3438bd6b69bb807d6a221d9
Anuitex2 <karyna.lisniak@gmail.com>
2019-02-25 17:49:03 +0200

    Initial Commit

---
+++ NestApp/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace NestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var configurator = new ElasticSearchConfigurator();
+            configurator.SearchByName("Polly");
+
+            //Person person = new Person()
+            //{
+            //    Id = 6, 
+            //    Name = "Polly",
+            //    Age = 3
+            //};
+            //configurator.IndexPerson(person);
+
+            //Person person1 = new Person()
+            //{
+            //    Id = 7,
+            //    Name = "Jolly",
+            //    Age = 15
+            //};
+            //Person person2 = new Person()
+            //{
+            //    Id = 8,
+            //    Name = "Holly",
+            //    Age = 22
+            //};
+            //configurator.IndexPeople(new List<Person>() { person1, person2 });
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NestApp/ElasticSearchConfigurator.cs
@@ -0,0 +1,60 @@
+﻿using Nest;
+using NestApp.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace NestApp
+{
+    public class ElasticSearchConfigurator
+    {
+        private readonly ConnectionSettings _settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+            .DefaultIndex("users");
+        private readonly ElasticClient _client;
+
+        public ElasticSearchConfigurator()
+        {
+            _client = new ElasticClient(_settings);
+        }
+
+        public void IndexPerson(Person person)
+        {
+            var indexResponse = _client.IndexDocument(person);
+        }
+
+        public void IndexPeople(List<Person> people)
+        {
+            var indexResponse = _client.IndexMany(people);
+        }
+
+        public void SearchByName(string name)
+        {
+            var response = _client.Search<Person>(s => s
+                .From(0)
+                .Size(10)
+                .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.Name)
+                        .Query(name)
+                    )
+                )
+            );
+
+            var result = response.Documents;
+        }
+
+        public void AggregateByAge()
+        {
+            var response = _client.Search<Person>(s => s
+                .From(0)
+                .Size(10)
+                .Aggregations(a => a
+                    .Terms("age", t => t
+                        .Field(f => f.Age)
+                    )
+                )
+            );
+
+            var result = response.Aggregations.Terms("age");
+        }
+    }
+}
